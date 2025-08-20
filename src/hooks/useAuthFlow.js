2fc0a5eb3e1ebb8e6c12991fc5cbb2f8216954a5
thef4tdaddy <github@ed-david.com>
2025-08-20 10:05:40 -0500

    debug: add console log to verify debug version is running

--- src/hooks/useAuthFlow.js
+++ src/hooks/useAuthFlow.js
@@ -25,7 +25,7 @@ const useAuthFlow = () => {
   const handleSetup = useCallback(
     async (userData) => {
       logger.auth("Layout handleSetup called", { hasUserData: !!userData });
-      console.log("🚨 DEBUG VERSION 2: useAuthFlow.js with debug logging is running!");
+      logger.auth("🚨 DEBUG VERSION 2: useAuthFlow.js with debug logging is running!");
       try {
         // ALWAYS generate budgetId deterministically from password for cross-device sync
         const { encryptionUtils } = await import("../utils/encryption");
