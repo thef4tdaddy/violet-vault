c26b417774f7961948e5fcd81a8f3046e5f3dbe4
thef4tdaddy <github@ed-david.com>
2025-11-06 18:08:57 -0600

    fix(ui): use X icon for deleting savings goals

--- src/components/savings/SavingsGoalCard.tsx
+++ src/components/savings/SavingsGoalCard.tsx
@@ -73,7 +73,7 @@ const SavingsGoalCard = ({ goal, onEdit, onDelete, priorities }) => {
             className="p-2 text-red-600 bg-red-50 hover:bg-red-100 border border-red-200 hover:border-red-300 rounded-lg transition-all"
             title="Delete goal"
           >
-            {React.createElement(getIcon("Trash"), { className: "h-4 w-4" })}
+            {React.createElement(getIcon("X"), { className: "h-4 w-4" })}
           </Button>
         </div>
       </div>
