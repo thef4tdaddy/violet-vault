c9d1a430b5a3b9e648fe45d9dee8a12d40ebc737
thef4tdaddy <github@ed-david.com>
2025-11-27 10:36:01 -0600

    chore: export input sanitization utilities from security index

--- src/utils/security/index.ts
+++ src/utils/security/index.ts
@@ -1,3 +1,4 @@
 export * as encryption from "./encryption.ts";
 export * as keyExport from "./keyExport.ts";
 export * as errorViewer from "./errorViewer.ts";
+export * from "./inputSanitization.ts";
